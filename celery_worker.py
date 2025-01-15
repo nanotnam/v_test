d1547ab470bc87a0573f80aff9604bc10cd262c3
nanotnam <vuhoangnam.kd@gmail.com>
2025-01-15 20:58:30 +0700

    Update celery_worker.py

--- celery_worker.py
+++ celery_worker.py
@@ -4,22 +4,16 @@ import os
 
 celery = Celery('celery_worker', broker='pyamqp://guest@localhost//', backend='rpc://')
 
-# Load model once to avoid reloading for every task
 model = load_model()
-
 
 @celery.task(bind=True)
 def run_inference(self, image_path):
-    try:
-        # Run inference
-        mask = predict(model, image_path)
+    
+    mask = predict(model, image_path)
 
-        # Save binary mask
-        output_path = os.path.join("static", "output_" + os.path.basename(image_path))
-        mask.save(output_path)
+    output_path = os.path.join("static", "output_" + os.path.basename(image_path))
+    mask.save(output_path)
 
-        return image_path, output_path
-    except Exception as e:
-        self.update_state(state='FAILURE', meta={'error': str(e)})
-        raise
+    return image_path, output_path
+
 
